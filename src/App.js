6995d9e85736fae6eed82214116e7cfdcec4f338
vadimMz <42109255+vadimMz@users.noreply.github.com>
2020-03-09 19:39:30 +0700

    Updated structure

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import Profile from "./components/profile/Profile";
+import Dialogs from "./components/Dialogs/DIalogs";
 
 function App() {
     return (
         <div className='app-wrapper'>
-           <Header/>
-           <Nav/>
-           <Profile/>
+            <Header/>
+            <Nav/>
+            <div className='app-wrapper-content'>
+
+                <Dialogs/>
+            </div>
+
+            {/*<Profile/>*/}
         </div>
     );
 }
