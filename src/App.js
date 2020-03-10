a2702bd8b8653de6ee7726001b430b61879ae99b
vadimMz <42109255+vadimMz@users.noreply.github.com>
2020-03-10 22:16:25 +0700

    Added react-router-dom module; created nav items (js + css);

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
+import Header from "./components/Header/Header";
+import Nav from "./components/Nav/Nav";
 import Profile from "./components/profile/Profile";
 import Dialogs from "./components/Dialogs/DIalogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import Messages from "./components/Messages/Messages";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Nav/>
-            <div className='app-wrapper-content'>
-
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Nav/>
+                <div className='app-wrapper-content'>
+                    <Route path='/Profile'> <Profile/></Route>
+                    <Route path='/Dialogs'> <Dialogs/></Route>
+                    <Route path='/Messages'> <Messages/></Route>
+                    <Route path='/News'> <News/></Route>
+                    <Route path='/Music'> <Music/></Route>
+                    <Route path='/Settings'> <Settings/></Route>
+                </div>
             </div>
-
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import styles from './Nav.module.css';
+import {Link} from "react-router-dom";
 
 function Nav() {
     return (
         <nav className={styles.nav}>
+
+
             <div className = {styles.item}>
-                <a>Profile</a>
+                <Link to='/Profile' path ='Profile'>Profile</Link>
             </div>
 
             <div className = {styles.item}>
-                <a>Messages</a>
+                <Link to='/Dialogs' path ='Dialogs'>Dialogs</Link>
             </div>
 
             <div className = {styles.item}>
-                <a>News</a>
+                <Link to='/Messages' path ='Messages'>Messages</Link>
             </div>
 
             <div className = {styles.item}>
-                <a>Music</a>
+                <Link to='/News' path ='News'>News</Link>
             </div>
 
             <div className = {styles.item}>
-                <a>Settings</a>
+                <Link to='/Music' path ='Music'>Music</Link>
+            </div>
+
+            <div className = {styles.item}>
+                <Link to='/Settings' path ='Settings'>Settings</Link>
             </div>
         </nav>
     );
