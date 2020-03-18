7c66e907ec181d2f50ff8a2c28929652841067b4
vadimMz <42109255+vadimMz@users.noreply.github.com>
2020-03-18 21:11:44 +0700

    added textarea el in dialogs

--- src/components/Dialogs/DIalogs.js
+++ src/components/Dialogs/DIalogs.js
@@ -5,7 +5,13 @@ import Message from "./Message/Message";
 
 
 const Dialogs = (props) => {
-debugger;
+    let textAreaElement = React.createRef();
+
+
+
+
+
+
     const FillDialogs = props.dialogs.map(elem => {
         return (
             <Dialog id={elem.id} name={elem.name}/>
@@ -27,7 +33,16 @@ debugger;
 
             <div className={styles.Messages}>
                 {FillMessages}
+                <div>
+                    <textarea ref={textAreaElement}></textarea>
+
+                    <div className={styles.NewMessageInput}>
+                        <button className= {styles.addNewMessageButton}> Add message</button>
+                    </div>
+                </div>
             </div>
+
+
 
         </div>
     )
