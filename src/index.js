5a6ed7492331d8491ccc06608cef1576a489c70e
vadimMz <42109255+vadimMz@users.noreply.github.com>
2020-03-24 22:33:14 +0700

    wrapped state.js to store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
+import store from './redux/state'
+
+
+
+export let rerenderEntireTree = (state) => {
+
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state}
+                 addPost={store.AddPost.bind(store)}
+                 updatePost = {store.UpdatePost.bind(store)}
+                 addMessage = {store.AddMessage.bind(store)}
+                 updateMessage = {store.UpdateMessage.bind(store)}
+            />
+        </BrowserRouter>, document.getElementById('root'));
+}
+
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
+
+
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
 import state, {AddPost, AddMessage, UpdateMessage, UpdatePost, subscribe} from "./redux/state";
 
 
@@ -22,4 +51,4 @@ export let rerenderEntireTree = (state) => {
 
 rerenderEntireTree(state);
 
-subscribe(rerenderEntireTree);
+subscribe(rerenderEntireTree);*/
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,85 @@
-let  rerenderEntireTree = () => {
+
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Привет!"},
+                {id: 2, message: "Че не отвечаешь?!"},
+                {id: 3, message: "Купи паскод!"}
+            ],
+            newPostText: "" //Text from textarea in profile.
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Vadim'},
+                {id: 2, name: 'Kate'},
+                {id: 3, name: 'Putin'},
+                {id: 4, name: 'Vasily'},
+                {id: 5, name: 'Oleg'}
+            ],
+            messages: [
+                {id: 1, message: 'Hey'},
+                {id: 2, message: 'how are you?'},
+                {id: 3, message: 'OK'},
+                {id: 4, message: 'Go'},
+            ],
+            newMessageText: ""  //Text from textarea in Dialogs/message.
+        },
+        sidebar: {
+            friends: [
+                {id: 1, name: 'Andrew'},
+                {id: 2, name: 'Alexey'},
+                {id: 3, name: 'Pauleg'}
+            ]
+        }
+    },
+    setState(state) {
+     this._state = state;
+    },
+    getState(){
+     return this._state;
+    },
+
+    _callSubscriber(){
+        console.log('state changed');
+    },
+    AddPost(postMessage){
+        let newPost = {
+            id: 4,
+            message: postMessage
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    UpdatePost(NewText){
+        this._state.profilePage.newPostText = NewText;
+        this._callSubscriber( this.state);
+    },
+    AddMessage(messageText){
+        let newMessage = {
+            id: 5,
+            message: messageText
+        };
+        this._state.dialogsPage.messages.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber( this.state);
+    },
+    UpdateMessage(text){
+        this._state.dialogsPage.newMessageText = text;
+        this._callSubscriber( this.state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer;
+    }
+};
+
+export default store;
+window.store = store;
+
+
+/*
+let  _callSubscriber = () => {
     console.log('state changed');
 }
 
@@ -9,11 +90,11 @@ export const AddPost = (postMessage) => {
     };
     state.profilePage.posts.push(newPost);
     state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
+    _callSubscriber(state);
 };
 export const UpdatePost = (NewText) => {
     state.profilePage.newPostText = NewText;
-    rerenderEntireTree(state);
+    _callSubscriber(state);
 };
 export const AddMessage = (messageText) => {
     let newMessage = {
@@ -22,15 +103,15 @@ export const AddMessage = (messageText) => {
     };
     state.dialogsPage.messages.push(newMessage);
     state.dialogsPage.newMessageText = '';
-    rerenderEntireTree(state);
+    _callSubscriber(state);
 };
 export const UpdateMessage = (text) => {
     state.dialogsPage.newMessageText = text;
-    rerenderEntireTree(state);
+    _callSubscriber(state);
 };
 
 export let subscribe  = (observer) => {
-    rerenderEntireTree = observer;
+    _callSubscriber = observer;
 }
 
 
@@ -67,4 +148,4 @@ let state = {
         ]
     }
 };
-export default state;
+export default state;*/
