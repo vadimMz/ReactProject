dda76e648e54e4086d81efbaad81604b6a9e7c98
vadimMz <42109255+vadimMz@users.noreply.github.com>
2020-03-30 13:29:40 +0700

    refactored reducers to switch case

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,18 +2,23 @@ const dialogsReducer = (state, action) => {
 
     const ADD_MESSAGE = 'ADD-MESSAGE';
     const UPDATE_MESSAGE = 'UPDATE-MESSAGE';
-        if (action.type === ADD_MESSAGE) {
+
+
+    switch (action.type) {
+        case ADD_MESSAGE:
             let newMessage = {
                 id: 5,
                 message: action.messageText //
             };
             state.messages.push(newMessage);
             state.newMessageText = '';
-
-        } else if (action.type === UPDATE_MESSAGE) {
+        case UPDATE_MESSAGE:
             state.newMessageText = action.messageText;
-        }
+        default:
+            return state;
 
-        return state;
-    };
+
+    }
+
+};
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -2,17 +2,20 @@ const profileReducer = (state, action) => {
     const ADD_POST = 'ADD-POST';
     const UPDATE_POST = 'UPDATE-POST';
 
-    if (action.type === ADD_POST) {
-        let newPost = {
-            id: 4,
-            message: action.postMessage //
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';
-
-    } else if (action.type === UPDATE_POST) {
-        state.newPostText = action.NewText;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 4,
+                message: action.postMessage //
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';
+        case UPDATE_POST:
+            state.newPostText = action.NewText;
+        default:
+            return state;
     }
-    return state;
 };
+
+
 export default profileReducer;
